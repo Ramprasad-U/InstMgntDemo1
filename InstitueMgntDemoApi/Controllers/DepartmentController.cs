bc8cb69b550d8fc559271d1a6d0b70e0de9c19a3
Ramprasad-U <ramuda789@gmail.com>
2021-10-01 20:57:11 +0530

    Add second commit

---
+++ InstitueMgntDemoApi/Controllers/DepartmentController.cs
@@ -0,0 +1,57 @@
+﻿using InstitueMgntDemoApi.Services;
+using InstitueMgntDemoApiData;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InstitueMgntDemoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IDepartmentRepository _departmentRepository;
+
+        public DepartmentController(IDepartmentRepository departmentRepository)
+        {
+            this._departmentRepository = departmentRepository;
+        }
+        //[EnableCors]
+        [HttpGet]
+        // public async Task<ActionResult<IEnumerable<Department>>> GetDepartments()
+        public async Task<ActionResult> GetDepartments()
+        {
+            try
+            {
+                return Ok(await _departmentRepository.GetDepartments());
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error retrieving data from the database");
+            }
+        }
+        [HttpGet("{id:int}", Name ="GetDepartmentById")]
+        public async Task<ActionResult<Department>> GetDepartmentById(int id)
+        {
+            try
+            {
+                var result = (await _departmentRepository.GetDepartmentById(id));
+                if(result == null)
+                {
+                    return NotFound();
+                }
+                return Ok(result);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error retrieving data from the database");
+            }
+        }
+    }
+}
